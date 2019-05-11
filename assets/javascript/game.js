067e3442b4cbecb81402ebdbbb38f11415d6693e
Zachary Dunn <zacharydunn@Zacharys-MBP.fios-router.home>
2019-05-11 15:54:32 -0400

    test4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,24 @@
 <script type="text/javascript">
+    var sharkType = ["baby", "daddy", "mommy", "doggy", "grandma", "grandpa"];
+    var gameAnswer = "";
+        sharkType = gameAnswer
+    document.onkeyup = function() {
+        var userGuess = String.fromCharCode(event.keyCode);
+            toLowerCase();
+        console.log(userGuess)
 
+        var maxGuess = numArr[0];
+            for (var i = 1; i < numArr.length; i++) {
+                if (numArr[i] > maxGuess) {
+                    maxGuess = numArr[i];
+                }
+            }
 
-    var options = [
+            return maxGuess;
+        };
 
+        var gameAnswer = sharkType [1, 2, 3];
+        console.log(findMax(gameAnswer));
 
-
+    
 </script>
